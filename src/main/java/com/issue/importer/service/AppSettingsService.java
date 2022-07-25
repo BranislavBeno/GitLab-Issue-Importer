a85649525834eba05e22503819bc62ec30041824
Branislav Beno <brano.beno@gmail.com>
2022-07-25 21:48:19 +0200

    Improve logging

--- src/main/java/com/issue/importer/service/AppSettingsService.java
+++ src/main/java/com/issue/importer/service/AppSettingsService.java
@@ -2,11 +2,18 @@ package com.issue.importer.service;
 
 import com.issue.importer.domain.ApplicationSettings;
 import com.issue.importer.io.props.SettingsReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public record AppSettingsService(SettingsReader reader) {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppSettingsService.class);
+
     public ApplicationSettings readApplicationSettings(MultipartFile file) {
-        return reader.provideSettings(file);
+        ApplicationSettings settings = reader.provideSettings(file);
+        LOGGER.info("PROPERTIES file '{}' was read successfully", file.getOriginalFilename());
+
+        return settings;
     }
 }
