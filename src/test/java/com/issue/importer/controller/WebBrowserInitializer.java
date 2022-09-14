eef07b108bbed1364ef9823feb7504ede2bf68b6
Branislav Beňo <brano.beno@gmail.com>
2022-09-14 23:49:13 +0200

    Make project arm64 compliant

--- src/test/java/com/issue/importer/controller/WebBrowserInitializer.java
+++ src/test/java/com/issue/importer/controller/WebBrowserInitializer.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.Map;
 
@@ -17,7 +18,10 @@ class WebBrowserInitializer {
     public static final BrowserWebDriverContainer<?> WEB_DRIVER_CONTAINER = populateWebDriver();
 
     private static BrowserWebDriverContainer<?> populateWebDriver() {
-        try (BrowserWebDriverContainer<?> driver = new BrowserWebDriverContainer<>()) {
+        try (BrowserWebDriverContainer<?> driver = new BrowserWebDriverContainer<>(
+                DockerImageName.parse("seleniarm/standalone-firefox:100.0.2-20220728")
+                        .asCompatibleSubstituteFor("selenium/standalone-firefox"))
+        ) {
             return driver.
                     withCapabilities(new FirefoxOptions()
                             .addArguments("--no-sandbox")
